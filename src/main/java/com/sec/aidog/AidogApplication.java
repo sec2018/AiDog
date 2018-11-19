88b7b597924ae4c00ea866964c51f1f4ec0f9fb6
shuye520com <shuye520com@163.com>
2018-11-19 12:46:00 +0800

    增加项目

---
+++ src/main/java/com/sec/aidog/AidogApplication.java
@@ -0,0 +1,12 @@
+package com.sec.aidog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AidogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AidogApplication.class, args);
+	}
+}
