3a4e7fba3de7790e71382bf54d823deed7ba0c22
shuye520com <shuye520com@163.com>
2019-03-15 16:42:15 +0800

    智能项圈2期  04牧犬登记

---
+++ src/main/resources/adminlte/pages/ui_js/dogregister.js
@@ -0,0 +1,97 @@
+$(function () {
+    //注册相关
+    var datares = {};
+    $.ajax({
+        url: "/aidog/api/register/4",
+        async:false,
+        type: "GET",
+        success: function (data) {
+            datares = eval("(" + data + ")");
+        }
+    });
+
+    $("#select_province").on('change', function () {
+        var selectvalue = $(this).find('option:selected').val();
+        selectvalue = selectvalue.substring(0,2);
+        $("#select_city").find("option").remove();
+        var select_city = document.getElementById("select_city");
+        select_city.options.add(new Option("请选择", "-1"));
+        if ($('#modalselect_adminlevel').find('option:selected').text()!="省级") {
+            for (var i = 0; i < datares.data1.length; i++) {
+                if (datares.data1[i].districtcode.toString().substring(0, 2) == selectvalue) {
+                    //遍历后台传回的结果，一项项往select中添加option
+                    select_city.options.add(new Option(datares.data1[i].districtname, datares.data1[i].districtcode));
+                }
+            }
+        }
+        $("#select_county").find("option").remove();
+        var select_county = document.getElementById("select_county");
+        select_county.options.add(new Option("请选择", "-1"));
+
+        $("#select_village").find("option").remove();
+        var select_village = document.getElementById("select_village");
+        select_village.options.add(new Option("请选择", "-1"));
+
+        $("#select_hamlet").find("option").remove();
+        var select_hamlet = document.getElementById("select_hamlet");
+        select_hamlet.options.add(new Option("请选择", "-1"));
+    });
+    $("#select_city").on('change', function () {
+        var selectvalue = $(this).find('option:selected').val();
+        $("#select_county").find("option").remove();
+        selectvalue = selectvalue.substring(0, 4);
+        var select_county = document.getElementById("select_county");
+        select_county.options.add(new Option("请选择", "-1"));
+        if ($('#modalselect_adminlevel').find('option:selected').text() != "市级") {
+            for (var i = 0; i < datares.data2.length; i++) {
+                if (datares.data2[i].districtcode.toString().substring(0, 4) == selectvalue) {
+                    //遍历后台传回的结果，一项项往select中添加option
+                    select_county.options.add(new Option(datares.data2[i].districtname, datares.data2[i].districtcode));
+                }
+            }
+        }
+        $("#select_village").find("option").remove();
+        var select_village = document.getElementById("select_village");
+        select_village.options.add(new Option("请选择", "-1"));
+
+        $("#select_hamlet").find("option").remove();
+        var select_hamlet = document.getElementById("select_hamlet");
+        select_hamlet.options.add(new Option("请选择", "-1"));
+    });
+    $("#select_county").on('change', function () {
+        var selectvalue = $(this).find('option:selected').val();
+        $("#select_village").find("option").remove();
+        selectvalue = selectvalue.substring(0, 6);
+        var select_village = document.getElementById("select_village");
+        select_village.options.add(new Option("请选择", "-1"));
+        if ($('#modalselect_adminlevel').find('option:selected').text() != "县级") {
+            for (var i = 0; i < datares.data3.length; i++) {
+                if (datares.data3[i].districtcode.toString().substring(0, 6) == selectvalue) {
+                    //遍历后台传回的结果，一项项往select中添加option
+                    select_village.options.add(new Option(datares.data3[i].districtname, datares.data3[i].districtcode));
+                }
+            }
+        }
+        $("#select_hamlet").find("option").remove();
+        var select_hamlet = document.getElementById("select_hamlet");
+        select_hamlet.options.add(new Option("请选择", "-1"));
+    });
+    $("#select_village").on('change', function () {
+        var selectvalue = $(this).find('option:selected').val();
+        $("#select_hamlet").find("option").remove();
+        selectvalue = selectvalue.substring(0, 9);
+        var select_hamlet = document.getElementById("select_hamlet");
+        select_hamlet.options.add(new Option("请选择", "-1"));
+        if ($('#modalselect_adminlevel').find('option:selected').text() != "乡级") {
+            for (var i = 0; i < datares.data4.length; i++) {
+                if (datares.data4[i].districtcode.toString().substring(0, 9) == selectvalue) {
+                    //遍历后台传回的结果，一项项往select中添加option
+                    select_hamlet.options.add(new Option(datares.data4[i].districtname, datares.data4[i].districtcode));
+                }
+            }
+        }
+    });
+
+
+
+})
