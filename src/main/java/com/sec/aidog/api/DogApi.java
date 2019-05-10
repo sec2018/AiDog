01b8d1bc6d7769fad944c78f74026678c4fed73a
shuye520com <shuye520com@163.com>
2019-05-10 16:37:20 +0800

    智能  界面修改8

--- src/main/java/com/sec/aidog/api/DogApi.java
+++ src/main/java/com/sec/aidog/api/DogApi.java
@@ -11,13 +11,21 @@ import com.sec.aidog.util.JsonResult;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Row;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import net.sf.json.JSONObject;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -519,4 +527,38 @@ public class DogApi {
         return ResponseEntity.ok(r);
     }
 
+
+    // excel导入
+    @ApiOperation(value = "从excel中批量注册牧犬及犬主人", notes = "从excel中批量注册牧犬及犬主人")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "token", value = "通行证", required = true, dataType = "String",paramType = "header")
+    })
+    @RequestMapping(value="batchdogregexcel",method = RequestMethod.POST)
+    @ResponseBody
+    public void export(@RequestParam("file") MultipartFile file,
+                       HttpServletRequest request, HttpServletResponse response) {
+        try {
+            // @RequestParam("file") MultipartFile file 是用来接收前端传递过来的文件
+            // 1.创建workbook对象，读取整个文档
+            InputStream inputStream = file.getInputStream();
+            POIFSFileSystem poifsFileSystem = new POIFSFileSystem(inputStream);
+            HSSFWorkbook wb = new HSSFWorkbook(poifsFileSystem);
+            // 2.读取页脚sheet
+            HSSFSheet sheetAt = wb.getSheetAt(0);
+            // 3.循环读取某一行
+            for (Row row : sheetAt) {
+                // 4.读取每一行的单元格
+                String stringCellValue1 = row.getCell(0).getStringCellValue(); // 第一列数据
+                String stringCellValue2 = row.getCell(1).getStringCellValue();// 第二列
+                // 写多少个具体看大家上传的文件有多少列.....
+                // 测试是否读取到数据,及数据的正确性
+                System.out.println(stringCellValue1);
+                System.out.println(stringCellValue2);
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
