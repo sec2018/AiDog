da8231e0156ba9cfae6e4e309f7b5ba20b2ea6a7
shuye520com <shuye520com@163.com>
2019-06-27 17:48:10 +0800

    智能  界面修改32 界面调整

--- src/main/resources/adminlte/pages/ui_js/neclist.js
+++ src/main/resources/adminlte/pages/ui_js/neclist.js
@@ -410,38 +410,37 @@ $(function () {
 
 
     // Array to track the ids of the details displayed rows
-    var detailRows = [];
-
-    $('#tbody').on( 'click', 'tr td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = dt.row( tr );
-        var idx = $.inArray( tr.attr('id'), detailRows );
-
-        if ( row.child.isShown() ) {
-            tr.removeClass( 'details' );
-            row.child.hide();
-
-            // Remove from the 'open' array
-            detailRows.splice( idx, 1 );
-        }
-        else {
-            tr.addClass( 'details' );
-            row.child( format( row.index() ) ).show();
-
-            // Add to the 'open' array
-            if ( idx === -1 ) {
-                detailRows.push( tr.attr('id') );
-            }
-        }
-    } );
+    // var detailRows = [];
+    //
+    // $('#tbody').on( 'click', 'tr td.details-control', function () {
+    //     var tr = $(this).closest('tr');
+    //     var row = dt.row( tr );
+    //     var idx = $.inArray( tr.attr('id'), detailRows );
+    //
+    //     if ( row.child.isShown() ) {
+    //         tr.removeClass( 'details' );
+    //         row.child.hide();
+    //
+    //         // Remove from the 'open' array
+    //         detailRows.splice( idx, 1 );
+    //     }
+    //     else {
+    //         tr.addClass( 'details' );
+    //         row.child( format( row.index() ) ).show();
+    //
+    //         // Add to the 'open' array
+    //         if ( idx === -1 ) {
+    //             detailRows.push( tr.attr('id') );
+    //         }
+    //     }
+    // } );
 
     // On each draw, loop over the `detailRows` array and show any child rows
-    dt.on( 'draw', function () {
-        $.each( detailRows, function ( i, id ) {
-            $('#'+id+' td.details-control').trigger( 'click' );
-        } );
-    });
-
+    // dt.on( 'draw', function () {
+    //     $.each( detailRows, function ( i, id ) {
+    //         $('#'+id+' td.details-control').trigger( 'click' );
+    //     } );
+    // });
 
 
     function format ( index ) {
